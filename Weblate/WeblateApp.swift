76250b6879de99e0427b0a6698b76413e9a2d3d6
Nathan Fallet <contact@nathanfallet.me>
2022-04-19 14:08:30 +0200

    Initial Commit

---
+++ Weblate/WeblateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeblateApp.swift
+//  Weblate
+//
+//  Created by Nathan Fallet on 19/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeblateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
